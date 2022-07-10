dd318650926982254c96f9226f96a8d7871c2aed
Tim W James <38764646+Tim-W-James@users.noreply.github.com>
2022-07-10 10:33:32 +1000

    Updated footer

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,11 @@
 import logo from "@assets/svg/logo.svg";
 import { BsLinkedin } from "react-icons/bs";
-import styles from "./Home.scss";
+import styles from "./Home.module.scss";
 
 const Home: React.FC = () => (
-  <div className={styles.home}>
+  <div className={styles["home"]}>
     <header>
-      <img src={logo} className="home-logo" alt="logo" />
+      <img src={logo} className={styles["home-logo"]} alt="logo" />
       <h1>Coming Soon...</h1>
       <a href="https://www.linkedin.com/in/timothy-william-james/">
         <BsLinkedin /> Find me on Linkedin
