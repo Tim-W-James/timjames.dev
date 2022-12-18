f0507cb23156fbbfb6f6a4f68699b044d57490c4
Tim W James <38764646+Tim-W-James@users.noreply.github.com>
2022-12-18 19:43:35 +1100

    feat: fix nested apply

--- .postcssrc.cjs
+++ .postcssrc.cjs
@@ -1,12 +1,12 @@
 module.exports = {
-  plugins: [
-    require("tailwindcss"),
-    require("autoprefixer"),
-    require("postcss-ts-classnames")({
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+    "postcss-ts-classnames": {
       dest: "src/styles/cssClasses.d.ts",
       // Set isModule if you want to import ClassNames from another file
       isModule: true,
       exportAsDefault: true, // to use in combination with isModule
-    }),
-  ],
+    },
+  },
 };
