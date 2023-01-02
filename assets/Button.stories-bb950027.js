5f96af7e7d1e857f5d9096e52cb7672751f0e318
Tim-W-James <tim.james9800@gmail.com>
2023-01-02 01:05:33 +0000

    Deploying to gh-pages from @ Tim-W-James/timjames.dev@086ee323ca66deb8eac7d1c7c486c78e2112b343 🚀

---
+++ assets/Button.stories-bb950027.js
@@ -0,0 +1,7 @@
+import{j as t,c as r}from"./cssUtils-31b06496.js";import{b as s}from"./index.esm-d05c6232.js";import{B as o}from"./Button-716ae357.js";import"./index-f1f749bf.js";import"./_commonjsHelpers-042e6b4d.js";import"./iconBase-4e3618d4.js";import"./useMediaQuery-f3cc2322.js";import"./mouseHover-f6e29944.js";import"./index-e4cb6166.js";const u={title:"Components/Button",component:o},e=()=>t.jsxs("div",{className:r("flex gap-4 items-center p-8","flex-row"),children:[t.jsx(o,{label:"dark",to:"https://timjames.dev/"}),t.jsx(o,{isLight:!0,label:"light",to:"https://timjames.dev/"}),t.jsx(o,{icon:t.jsx(s,{}),label:"icon",to:"https://timjames.dev/"}),t.jsx(o,{icon:t.jsx(s,{}),to:"https://timjames.dev/"})]});var i;e.parameters={...e.parameters,storySource:{source:`() => <div className={cn("flex gap-4 items-center p-8", "flex-row")}>
+    <ButtonComponent label="dark" to="https://timjames.dev/" />
+    <ButtonComponent isLight label="light" to="https://timjames.dev/" />
+    <ButtonComponent icon={<BsFillArrowRightCircleFill />} label="icon" to="https://timjames.dev/" />
+    <ButtonComponent icon={<BsFillArrowRightCircleFill />} to="https://timjames.dev/" />
+  </div>`,...(i=e.parameters)==null?void 0:i.storySource}};const x=["Button"];export{e as Button,x as __namedExportsOrder,u as default};
+//# sourceMappingURL=Button.stories-bb950027.js.map
