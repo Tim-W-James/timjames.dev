52640b734e213835ad70b7d9b323179a8184e11c
Tim W James <38764646+Tim-W-James@users.noreply.github.com>
2023-01-05 01:31:47 +1100

    chore: bump deps

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,31 +1,4 @@
 module.exports = {
   extends: ["@tim-w-james", "./.eslintrc-auto-import.json"],
-  plugins: ["vitest", "react-refresh"],
-  rules: {
-    "no-alert": "error",
-    "react-refresh/only-export-components": "warn",
-    "no-restricted-imports": "off",
-    "@typescript-eslint/no-restricted-imports": [
-      "warn",
-      {
-        name: "react-redux",
-        importNames: ["useSelector", "useDispatch"],
-        message:
-          "Use typed hooks `useAppDispatch` and `useAppSelector` instead.",
-      },
-    ],
-  },
-  overrides: [
-    {
-      // overrides for tests
-      files: ["*.test.{ts,tsx}"],
-    },
-    {
-      // override for storybook
-      files: ["*.{stories,story}.{ts,tsx,mdx}"],
-      rules: {
-        "@typescript-eslint/naming-convention": "off",
-      },
-    },
-  ],
+  plugins: ["vitest"],
 };
