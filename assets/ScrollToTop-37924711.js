69456dba75ee5d74613e562224af7585365a920d
Tim-W-James <tim.james9800@gmail.com>
2023-01-04 12:49:21 +0000

    Deploying to gh-pages from @ Tim-W-James/timjames.dev@86c45be7a31b45f0f5e0b397ac4a2ce9a9c82919 🚀

--- assets/ScrollToTop-37924711.js
+++
@@ -1,2 +0,0 @@
-import{r as t}from"./index-f1f749bf.js";import{u as r}from"./index-b8809eaa.js";const e=()=>{const{pathname:o}=r();return t.useEffect(()=>{window.location.hash||setTimeout(()=>{window.scrollTo({top:0,behavior:"smooth"})},0)},[o]),null};export{e as S};
-//# sourceMappingURL=ScrollToTop-37924711.js.map
