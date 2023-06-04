b7075adea04fd65686827b290fe69e9d30bd3e4c
Tim-W-James <tim.james9800@gmail.com>
2023-06-04 09:50:18 +0000

    Deploying to gh-pages from @ Tim-W-James/timjames.dev@3305fa0500463ab312354f33f2c65ceca9dd8044 🚀

---
+++ assets/Contact.stories-b3d33d03.js
@@ -0,0 +1,23 @@
+import{j as t,c as e}from"./cssUtils-c95a03d3.js";import{F as x}from"./Footer-cfda3890.js";import{N as h,C as s}from"./Navbar-91f502ed.js";import{P as j}from"./Page-e60a3997.js";import{S as u}from"./ScrollToTop-9c340e0a.js";import{C as T,c as b,P as f,a as S,h as C,T as g,b as O}from"./ContactForm-3cde3ee9.js";import{B as k}from"./index-c9ae3a6c.js";import{F as E}from"./index-bc2affff.js";import{b as R,c as w,d as A,e as F}from"./index.esm-32e9765f.js";import{M as v}from"./index.esm-0b1d0876.js";import{b as _,c as y}from"./index.esm-f5582ba4.js";import{H as P}from"./react-router-hash-link.esm-5e949a14.js";import{r as N}from"./index-8db94870.js";import"./useMediaQuery-4d2756a2.js";import"./iframe-97c8849c.js";import"../sb-preview/runtime.js";import"./logo-7f2f81ce.js";import"./mouseHover-f6e29944.js";import"./lib-0b8df509.js";import"./_commonjsHelpers-042e6b4d.js";import"./iconBase-3663c682.js";import"./Button-c3673857.js";import"./index.esm-5cca5c2b.js";import"./hoist-non-react-statics.cjs-814f66cf.js";import"./index-302a0035.js";import"./index-8ce4a492.js";const o=({icon:a,body:i,url:p,tooltip:d})=>t.jsx("a",{"aria-label":i,href:p,rel:"noreferrer",target:"_blank",title:d,children:t.jsxs("div",{className:e("hover:text-light-accent active:text-dark-accent active:underline","inline-flex","items-center gap-1"),children:[a,t.jsx("p",{className:e("hyphens-none"),children:i})]})}),B=()=>{const a=N.useCallback(()=>navigator.clipboard.writeText(`${location.href}#social-links`),[]);return t.jsx("div",{children:t.jsxs(E,{transitionDuration:200,children:[t.jsx(T,{onSubmit:b,showPromptOnClose:!0}),t.jsxs("h2",{className:e("mx-auto mt-8 w-fit self-center text-center"),id:"social-links",children:[t.jsxs(P,{className:e("hash-link"),onClick:a,to:"#social-links",children:["Social Links"," "]}),t.jsx("hr",{className:e("radial-border mb-4")})]}),t.jsxs("section",{"aria-labelledby":"social-links",className:e("mx-auto mt-8 grid w-fit gap-4 self-center text-center text-xl","grid-cols-2"),children:[t.jsx(o,{body:"LinkedIn",icon:t.jsx(R,{}),tooltip:"timothy-william-james",url:"https://www.linkedin.com/in/timothy-william-james/"}),t.jsx(o,{body:"GitHub",icon:t.jsx(w,{}),tooltip:"Tim-W-James",url:"https://github.com/Tim-W-James"}),t.jsx(o,{body:"Twitter",icon:t.jsx(A,{}),tooltip:"@TimWJames",url:"https://twitter.com/TimWJames"}),t.jsx(o,{body:"Facebook",icon:t.jsx(F,{}),tooltip:"TimJames9800",url:"https://www.facebook.com/TimJames9800/"}),t.jsx(o,{body:"Steam",icon:t.jsx(_,{}),tooltip:"ExplosiveFridge",url:"https://steamcommunity.com/id/ExplosiveFridge"}),t.jsx(o,{body:"Spotify",icon:t.jsx(y,{}),tooltip:"TimJ",url:"https://open.spotify.com/user/22xzbbohotkdpq5wfipvefk4y"})]}),t.jsxs("div",{className:e("mt-4 text-center text-xl","basis-min-content"),children:[t.jsx(v,{})," Send me an email:"," ",t.jsx("a",{className:e("link","hyphens-none"),href:"mailto:tim.james.work9800@gmail.com",rel:"noreferrer",target:"_blank",title:"tim.james.work9800@gmail.com",children:"tim.james.work9800@gmail.com"})]})]})})},l=B,ct={component:l,parameters:{backgrounds:{disable:!0},controls:{hideNoControlsWarning:!0}}},r=()=>t.jsx(f,{store:S({reducer:{contactForm:O}}),children:t.jsx(k,{children:t.jsxs(C,{container:{element:"captcha-container",parameters:{badge:"bottomright",theme:"dark"}},reCaptchaKey:"6LeSqcEjAAAAAFql9Z3tRjnKODIneEJoGJzlj0iJ",children:[t.jsx(u,{}),t.jsx(h,{}),t.jsx(j,{content:t.jsx(l,{}),description:s.description,title:s.title}),t.jsx(x,{allowFixed:!0}),t.jsx("div",{className:e("captcha-show"),id:"captcha-container"}),t.jsx(g,{})]})})});var n,c,m;r.parameters={...r.parameters,docs:{...(n=r.parameters)==null?void 0:n.docs,source:{originalSource:`() => <Provider store={configureStore({
+  reducer: {
+    contactForm: contactFormReducer
+  }
+})}>
+    <Router>
+      <GoogleReCaptchaProvider container={{
+      element: "captcha-container",
+      parameters: {
+        badge: "bottomright",
+        theme: "dark"
+      }
+    }} reCaptchaKey={{"STORYBOOK_SITE_RECAPTCHA_KEY":"6LeSqcEjAAAAAFql9Z3tRjnKODIneEJoGJzlj0iJ","BASE_URL":"./","MODE":"production","DEV":false,"PROD":true,"SSR":false,"STORYBOOK":"true"}["STORYBOOK_SITE_RECAPTCHA_KEY"] || ""}>
+        <ScrollToTop />
+        <Navbar />
+        <Page content={<ContactPage />} description={CONTACT.description} title={CONTACT.title} />
+        <Footer allowFixed />
+        <div className={cn("captcha-show")} id="captcha-container" />
+        <Toast />
+      </GoogleReCaptchaProvider>
+    </Router>
+  </Provider>`,...(m=(c=r.parameters)==null?void 0:c.docs)==null?void 0:m.source}}};const mt=["Contact"];export{r as Contact,mt as __namedExportsOrder,ct as default};
+//# sourceMappingURL=Contact.stories-b3d33d03.js.map
